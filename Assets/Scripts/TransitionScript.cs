2d361264ff4fb4ca004d88e3313769e1a9446538
Bullseye25 <burning_pain@live.com>
2017-05-22 19:58:06 +0500

    Scoring System, Is ready to use, however, the final end score are still in process,

--- Assets/Scripts/TransitionScript.cs
+++ Assets/Scripts/TransitionScript.cs
@@ -7,21 +7,25 @@ public class TransitionScript : MonoBehaviour
 {
     public static TransitionScript instance = null;
 
-	private int m_totalTurns = 7, m_turn = 0;
+	private int m_totalTurns = 7, m_turn = 0, m_amountOfStones = 4, m_end = 0, m_totalEnds = 10;
 	
-    private Text m_p1Score, m_p2Score;
+    private Text m_p1TotalScore, m_p2TotalScore, m_winningTitle;
+
+    public List<Text> m_p1EndScore = new List<Text>(), m_p2EndScore = new List<Text>();
 
 	private float m_radiusSize;
 
-	public List<GameObject> list = new List<GameObject>();
+    private List<GameObject> m_stonesInHouse = new List<GameObject>();
 
+    [Header("Stone Images Displayed On The Scene")]
     public List<GameObject> m_stonesUIImages = new List<GameObject>();
 
+	[Header("House")]
     public Transform m_house;
 
 	private List<GameObject> m_p1Stones = new List<GameObject>(), m_p2Stones = new List<GameObject>();
 
-	private int m_amountOfStones = 4;
+    private GameObject m_scoringPanel;
 
     internal void Awake()
 	{
@@ -33,13 +37,21 @@ public class TransitionScript : MonoBehaviour
 	{
 		m_radiusSize = m_house.parent.localScale.x;
 
-		m_p1Score = GameObject.Find("_P1Score").GetComponent<Text>();
+		m_p1TotalScore = GameObject.Find("_P1Score").GetComponent<Text>();
 
-		m_p2Score = GameObject.Find("_P2Score").GetComponent<Text>();
+		m_p2TotalScore = GameObject.Find("_P2Score").GetComponent<Text>();
 
         m_p1Stones = PoolingManager.instance.CreatePool(Pool.Stones_p1_Stone, m_amountOfStones);
 
         m_p2Stones = PoolingManager.instance.CreatePool(Pool.Stones_p2_Stone, m_amountOfStones);
+
+        m_p1EndScore = GetEndScoringTexts("P1_SCORING");
+
+		m_p2EndScore = GetEndScoringTexts("P2_SCORING");
+
+        m_winningTitle = GameObject.Find("PlayerNameText").GetComponent<Text>();
+
+        m_scoringPanel = GetScoringPanel();
     }
 
     //following mehtod will be called whenever there is a need to reset the properties of the stone
@@ -62,30 +74,34 @@ public class TransitionScript : MonoBehaviour
 
     public void NextTurn()
     {
-		if (m_turn < m_totalTurns)
-        {
-            ControllerScript.instance.NextTurn();
+        //if (m_end < m_totalEnds)
+        //{
+            if (m_turn < m_totalTurns)
+            {
+                ControllerScript.instance.NextTurn();
 
-			RemoveOneUIStone();
+                RemoveOneUIStone();
 
-            m_turn++;
+                m_turn++;
 
-        }
-        else if (m_turn >= m_totalTurns)
-        {
-			ControllerScript.instance.m_stoneClone = false;
+            }
+            else if (m_turn >= m_totalTurns)
+            {
+				m_end++;
 
-			ControllerScript.instance.NextTurn();
-            
-            GetClosestStones();
+                ControllerScript.instance.m_stoneClone = false;
 
-            CalculateScores();
+                ControllerScript.instance.NextTurn();
 
-            ResetTurn();
+                GetClosestStones();
 
-        }
+                CalculateScores();
 
-		Debug.Log("turn  : " + m_turn);
+                ResetTurn();
+
+                ActivateScoringPanel();
+            }
+        //}
     }
 
     private void RemoveOneUIStone()
@@ -105,7 +121,7 @@ public class TransitionScript : MonoBehaviour
 
         ActivateStonsUI();
 
-        list.Clear();
+        m_stonesInHouse.Clear();
 	}
 
     private void ActivateStonsUI()
@@ -124,15 +140,15 @@ public class TransitionScript : MonoBehaviour
         }
     }
 
-	internal void CalculateScores()
+	private void CalculateScores()
 	{
-		list.Sort(ByDistance);
+		m_stonesInHouse.Sort(ByDistance);
 
 		int p1 = 0, p2 = 0;
 
-		for (int i = 0; i < list.Count; i++)
+		for (int i = 0; i < m_stonesInHouse.Count; i++)
 		{
-			if (list[i].gameObject.name == "Stones_p1_Stone")
+			if (m_stonesInHouse[i].gameObject.name == "Stones_p1_Stone")
 			{
 				if (p2 == 0)
 					p1++;
@@ -151,19 +167,52 @@ public class TransitionScript : MonoBehaviour
 		DisplayScores(p1, p2);
 	}
 
-	internal void DisplayScores(int a_p1, int a_p2)
-	{
-		if (a_p1 > a_p2)
-		{
-			int p1 = System.Int32.Parse(m_p1Score.text) + a_p1;
-			m_p1Score.text = p1.ToString();
+    private void DisplayScores(int a_p1, int a_p2)
+    {
+        if (a_p1 > a_p2)
+        {
+            SetScore(a_p1, m_p1TotalScore, m_p1EndScore, m_p2EndScore);
+            SetWinningTitle("Player 1");
+        }
+        else
+        {
+            SetScore(a_p2, m_p2TotalScore, m_p2EndScore, m_p1EndScore);
+            SetWinningTitle("Player 2");
 		}
-		else
-		{
-			int p2 = System.Int32.Parse(m_p2Score.text) + a_p2;
-			m_p2Score.text = p2.ToString();
-		}
+
+        if (m_end == m_totalEnds)
+            DiclearFinalScore(a_p1, a_p2);
+    }
+
+    private void DiclearFinalScore(int a_p1, int a_p2)
+    {
+        int p1 = System.Int32.Parse(m_p1EndScore.ToString())+ a_p1;
+
+        int p2 = System.Int32.Parse(m_p2EndScore.ToString()) + a_p2;
+
+        if(p1 > p2)
+        {
+			m_winningTitle.text = "CONGRATULATIONS"+ "\n" + "PLAYER 1" + "\n" + "WINS" + "\n" + "MATCH";
+        }
+        else
+        {
+			m_winningTitle.text = "CONGRATULATIONS" + "\n" + "PLAYER 2" + "\n" + "WINS" + "\n" + "MATCH";
+        }
 	}
+
+    private void SetScore(int a_playerScore, Text a_totalScoringTextDisplay, List<Text> a_winnerEndScore, List<Text> a_loserEndScore)
+    {
+		int playerTotalScore = System.Int32.Parse(a_totalScoringTextDisplay.text) + a_playerScore;
+		a_totalScoringTextDisplay.text = playerTotalScore.ToString();
+		a_winnerEndScore[m_end].text = a_playerScore.ToString();
+        a_winnerEndScore[a_loserEndScore.Count-1].text = playerTotalScore.ToString();
+		a_loserEndScore[m_end].text = "0";
+	}
+
+    private void SetWinningTitle(string a_playerID)
+    {
+        m_winningTitle.text = a_playerID + "\n" + "WINS" + "\n" + "END " + m_end;
+    }
 
 	internal int ByDistance(GameObject a, GameObject b)
 	{
@@ -184,8 +233,8 @@ public class TransitionScript : MonoBehaviour
 
 			if (a_dis < m_radiusSize)
 			{
-				if (!list.Contains(_stone))
-					list.Add(_stone);
+				if (!m_stonesInHouse.Contains(_stone))
+					m_stonesInHouse.Add(_stone);
 			}
 		}
 	}
@@ -208,4 +257,52 @@ public class TransitionScript : MonoBehaviour
 	{
         return m_turn % 2 != 0;
 	}
+
+    private List<Text> GetEndScoringTexts(string a_parentObject)
+    {
+        List<Text> _list = new List<Text>();
+
+        GameObject m_endScoringPanel = GameObject.Find(a_parentObject);
+
+        foreach(Transform _score in m_endScoringPanel.transform)
+        {
+            _list.Add(_score.GetChild(0).GetComponent<Text>());
+        }
+
+        return _list;
+    }
+
+    private GameObject GetScoringPanel()
+    {
+        GameObject a_scoringPanel = GameObject.Find("_MainScoringCanvas");
+		a_scoringPanel.transform.SetParent(null);
+		a_scoringPanel.SetActive(false);
+
+        return a_scoringPanel;
+    }
+
+    private void ActivateScoringPanel()
+    {
+        m_scoringPanel.SetActive(true);
+    }
+
+    public void ClearScoreBoard()
+    {
+        if (m_end >= m_totalEnds)
+        {
+            ClearScoreBoard(m_p1TotalScore, m_p1EndScore);
+
+            ClearScoreBoard(m_p2TotalScore, m_p2EndScore);
+        }
+    }
+
+    private void ClearScoreBoard(Text a_displayedScore, List<Text> a_endScoreBoard)
+    {
+        a_displayedScore.text = "0";
+
+        for (int i = 1; i < a_endScoreBoard.Count; i++)
+        {
+            a_endScoreBoard[i].text = "-";
+        }
+    }
 }
